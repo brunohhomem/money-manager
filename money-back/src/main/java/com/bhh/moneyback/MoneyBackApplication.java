a0d61ebc4f161125ea30983bdbbde17938721dad
Bruno H. Homem <brunohhomem@gmail.com>
2024-03-30 15:42:11 -0300

    creation project backend

---
+++ money-back/src/main/java/com/bhh/moneyback/MoneyBackApplication.java
@@ -0,0 +1,13 @@
+package com.bhh.moneyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyBackApplication.class, args);
+	}
+
+}
---
+++ money-back/src/test/java/com/bhh/moneyback/MoneyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhh.moneyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
