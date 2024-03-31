e748146aff9286bdf6c73a5390492738c07a907c
Bruno H. Homem <brunohhomem@gmail.com>
2024-03-31 19:17:59 -0300

    added: gateway

---
+++ money-back/src/main/java/com/bhh/moneyback/domain/gateway/ActivityGateway.java
@@ -0,0 +1,14 @@
+package com.bhh.moneyback.domain.gateway;
+
+import com.bhh.moneyback.domain.activity.Activity;
+
+import java.util.List;
+
+public interface ActivityGateway {
+
+    public void create(final Activity anActivity);
+    public void delete(final String anId);
+    public List<Activity> findAll();
+
+
+}
